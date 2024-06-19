3108bbee61eafc9109b7f422b2ffe8b9e1f04aa8
NiyiHub <niyiblack@outlook.com>
2024-06-19 01:37:46 +0100

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <hi>Hello World! The Keeper app here.</hi>
+  </React.StrictMode>
+);
+
