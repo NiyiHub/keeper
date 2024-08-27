96a9f63eaf795a710f7d43263868194d25d1cc16
NiyiHub <niyiblack@outlook.com>
2024-08-27 18:50:27 +0100

    Created a constant that keeps track of the title and content.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Entry from "./Note";
-import notes from "../notes";
 import CreateArea from "./CreateArea";
 
 function App() {
@@ -10,13 +9,7 @@ function App() {
         <div className="html body">
             <Header />
             <CreateArea />
-            {notes.map((noteEntry) => (
-                    <Entry 
-                    key={noteEntry.key}
-                    title={noteEntry.title}
-                    content={noteEntry.content}
-                    />
-                ))}
+            <Entry key={1} title="Note title" content="Note content" />
             <Footer />
         </div>
     )
